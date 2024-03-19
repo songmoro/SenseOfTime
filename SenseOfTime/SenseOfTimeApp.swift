1830099de8b5843813713f34bce4ffd645c60825
songmoro <wlqgkr@naver.com>
2024-03-19 10:24:53 +0900

    Initial Commit

---
+++ SenseOfTime/SenseOfTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenseOfTimeApp.swift
+//  SenseOfTime
+//
+//  Created by 송재훈 on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SenseOfTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
