59a4e72ea7499600a5e93b4999f837db7ecda5b6
songmoro <wlqgkr@naver.com>
2024-03-20 18:49:05 +0900

    feat: 준비 뷰 구현'

--- SenseOfTime/ReadyView.swift
+++ SenseOfTime/ReadyView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct ReadyView: View {
+    @State var count = 5
+    @State var timer: Timer?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 15)
+                .frame(width: 250)
+                .foregroundStyle(.gray)
+            
+            Text(count.description)
+                .font(.largeTitle)
+        }
+        .onAppear {
+            count = 5
+            timer?.invalidate()
+            
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                if self.count > 1 {
+                    self.count -= 1
+                }
+                else {
+                    timer.invalidate()
+                }
+            }
+        }
+        .onDisappear {
+            timer?.invalidate()
+        }
     }
 }
 
