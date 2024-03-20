539d1160b3ae68e7a470d52db6cad4930d4da24a
songmoro <wlqgkr@naver.com>
2024-03-20 17:26:51 +0900

    feat: 메인 뷰 구현

--- SenseOfTime/MainView.swift
+++ SenseOfTime/MainView.swift
@@ -11,11 +11,24 @@ struct MainView: View {
     @State var second = 1
     
     var body: some View {
+        Spacer()
+        title
+        Spacer()
         picker
+        Spacer()
+    }
+    
+    @ViewBuilder
+    var title: some View {
+        Text("마음 속으로 숫자를 세어보세요")
+            .font(.title)
     }
     
     @ViewBuilder
     var picker: some View {
+        Text("몇 초까지 세어볼까요?")
+            .padding()
+        
         VStack(spacing: -20) {
             Image(systemName: "arrowtriangle.down.fill")
             
