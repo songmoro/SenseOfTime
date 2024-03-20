2943fbdbd9507acf2770a223199d39bced558b70
songmoro <wlqgkr@naver.com>
2024-03-20 09:56:35 +0900

    feat: 피커 뷰 -> 메인 뷰 이동 및 피커 뷰 삭제

--- SenseOfTime/MainView.swift
+++ SenseOfTime/MainView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct MainView: View {
+    @State var second = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        picker
+    }
+    
+    @ViewBuilder
+    var picker: some View {
+        VStack(spacing: -20) {
+            Image(systemName: "arrowtriangle.down.fill")
+            
+            Picker(selection: $second) {
+                ForEach(1..<301) { number in
+                    HStack {
+                        Text(number.description)
+                            .font(.footnote)
+                            .rotationEffect(.degrees(90))
+                        
+                        Rectangle()
+                            .frame(width: number % 5 == 0 ? 40 : 15, height: 5)
+                        Rectangle()
+                            .frame(width: number % 5 == 0 ? 0 : 25, height: 5)
+                            .foregroundStyle(.clear)
+                    }
+                    .tag(number)
+                }
+            } label: { }
+                .rotationEffect(.degrees(-90))
+                .frame(width: 100)
+                .pickerStyle(.wheel)
+                .scaleEffect(2.0)
+            
+            Image(systemName: "arrowtriangle.up.fill")
+        }
     }
 }
 
