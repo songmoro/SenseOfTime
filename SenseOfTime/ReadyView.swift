417375fb52b76ce1ef061483df6fba068c8417e0
songmoro <wlqgkr@naver.com>
2024-03-20 17:28:53 +0900

    feat: 준비 뷰 init

---
+++ SenseOfTime/ReadyView.swift
@@ -0,0 +1,18 @@
+//
+//  ReadyView.swift
+//  SenseOfTime
+//
+//  Created by 송재훈 on 3/20/24.
+//
+
+import SwiftUI
+
+struct ReadyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReadyView()
+}
