7500f3ef93afea5c50d58381dc87c138efc5edbd
songmoro <wlqgkr@naver.com>
2024-03-20 09:51:03 +0900

    feat: 메인 뷰 init

---
+++ SenseOfTime/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SenseOfTime
+//
+//  Created by 송재훈 on 3/20/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
