902e033c5372ee67dc19fce1ec26c0a04ceb9cd5
songmoro <wlqgkr@naver.com>
2024-03-20 09:46:49 +0900

    feat: 버튼 뷰 디버깅 코드 제거

--- SenseOfTime/ButtonView.swift
+++ SenseOfTime/ButtonView.swift
@@ -12,90 +12,42 @@ struct ButtonView: View {
     @State var status = Status.main
     
     var body: some View {
-        VStack {
-            HStack {
-                Button {
-                    withAnimation(.easeInOut) {
-                        status = .main
-                        isToggle = true
-                    } completion: {
-                        isToggle = false
-                    }
-                } label: {
-                    Text("Main")
-                }
+        HStack {
+            Button {
                 
-                Button {
-                    withAnimation(.easeInOut) {
-                        status = .ready
-                        isToggle = true
-                    } completion: {
-                        isToggle = false
-                    }
-                } label: {
-                    Text("Ready")
-                }
-                
-                Button {
-                    withAnimation(.easeInOut) {
-                        status = .play
-                        isToggle = true
-                    } completion: {
-                        isToggle = false
-                    }
-                } label: {
-                    Text("Play")
-                }
-                
-                Button {
-                    withAnimation(.easeInOut) {
-                        status = .end
-                        isToggle = true
-                    } completion: {
-                        isToggle = false
-                    }
-                } label: {
-                    Text("End")
-                }
+            } label: {
+                Image(systemName: "xmark.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+                    .rotationEffect(.degrees(isToggle ? 360 : 0))
+                    .foregroundStyle(status.buttonColor(.stop))
+                    .opacity(status.buttonOpacity(.stop))
             }
             
-            HStack {
+            ZStack {
                 Button {
                     
                 } label: {
-                    Image(systemName: "xmark.circle")
+                    Image(systemName: "stop.circle")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 50, height: 50)
+                        .frame(width: 60, height: 60)
                         .rotationEffect(.degrees(isToggle ? 360 : 0))
-                        .foregroundStyle(status.buttonColor(.stop))
-                        .opacity(status.buttonOpacity(.stop))
+                        .foregroundStyle(status.buttonColor(.pause))
+                        .opacity(status.buttonOpacity(.pause))
                 }
                 
-                ZStack {
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "stop.circle")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 60, height: 60)
-                            .rotationEffect(.degrees(isToggle ? 360 : 0))
-                            .foregroundStyle(status.buttonColor(.pause))
-                            .opacity(status.buttonOpacity(.pause))
-                    }
+                Button {
                     
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "play.circle")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 60, height: 60)
-                            .rotationEffect(.degrees(isToggle ? 360 : 0))
-                            .foregroundStyle(status.buttonColor(.play))
-                            .opacity(status.buttonOpacity(.play))
-                    }
+                } label: {
+                    Image(systemName: "play.circle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 60, height: 60)
+                        .rotationEffect(.degrees(isToggle ? 360 : 0))
+                        .foregroundStyle(status.buttonColor(.play))
+                        .opacity(status.buttonOpacity(.play))
                 }
             }
         }
